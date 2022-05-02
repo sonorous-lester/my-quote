c8e23af17a62bee3fa1b0553edd589a86148c476
Lester <sonorous.lester@gmail.com>
2022-05-02 14:37:37 +0800

    create register test suite

--- feature/register/delivery/http_test.go
+++ feature/register/delivery/http_test.go
@@ -4,8 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"myquote/domain"
 	"myquote/domain/register"
 	"myquote/service/logger"
 	"net/http"
@@ -22,24 +23,35 @@ func (m *MockedRegisterUsecase) Register(user register.NewUser) error {
 	return args.Error(0)
 }
 
-var l = logger.NewLogger("")
+type RegisterTestSuite struct {
+	suite.Suite
+	uc *MockedRegisterUsecase
+	l  domain.Logger
+	g  *gin.Engine
+	r  *httptest.ResponseRecorder
+}
 
-func TestRegisterSuccess(t *testing.T) {
+func (s *RegisterTestSuite) SetupTest() {
+	s.uc = new(MockedRegisterUsecase)
+	s.l = logger.NewLogger("")
+	s.g = gin.Default()
+	s.r = httptest.NewRecorder()
+}
+
+func (s *RegisterTestSuite) TestRegisterSuccess() {
 	newUser := register.NewUser{
 		Email:    "123@gmail.com",
 		Password: "123456",
 	}
 	body, _ := json.Marshal(newUser)
-
-	g := gin.Default()
-	mockedUc := new(MockedRegisterUsecase)
-	mockedUc.On("Register", newUser).Return(nil)
-	NewRegisterHTTPHandler(g, l, mockedUc)
-
-	w := httptest.NewRecorder()
+	s.uc.On("Register", newUser).Return(nil)
+	NewRegisterHTTPHandler(s.g, s.l, s.uc)
 	req, _ := http.NewRequest(http.MethodPost, "/api/register", bytes.NewBuffer(body))
 	req.Header.Set("Content-Type", "application/json")
-	g.ServeHTTP(w, req)
+	s.g.ServeHTTP(s.r, req)
+	s.Assert().Equal(http.StatusOK, s.r.Code)
+}
 
-	assert.Equal(t, http.StatusOK, w.Code)
+func TestRegisterHTTPHandler(t *testing.T) {
+	suite.Run(t, new(RegisterTestSuite))
 }
